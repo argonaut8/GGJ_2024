4c8c532fe963371d53c12f667e9e00784045244c
Theo <theonixie@gmail.com>
2024-01-26 13:46:44 -0800

    Improved cursor graphic, brush draws lines

--- src/tools/brush.gd
+++ src/tools/brush.gd
@@ -5,6 +5,7 @@ extends Tool
 @export var color: Color = Color.BLACK
 
 var is_clicked: bool = false
+var prev_mouse_position: Vector2
 
 func _init(canvas: Image) -> void:
 	super._init(canvas)
@@ -15,14 +16,18 @@ func _unhandled_input(event) -> void:
 		if event.get_button_index() == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				is_clicked = true
+				prev_mouse_position = event.position
 				edited_pixels.append(event.position.round() as Vector2i)
 				draw_pixels(edited_pixels)
 			else:
 				is_clicked = false
 	elif event is InputEventMouseMotion and is_clicked:
-		edited_pixels.append(event.position.round() as Vector2i)
+		var brushPosition = prev_mouse_position
+		while (brushPosition - event.position).length() > 0.0:
+			edited_pixels.append(brushPosition.round() as Vector2i)
+			brushPosition = brushPosition.move_toward(event.position, 1)
 		draw_pixels(edited_pixels)
-		
+		prev_mouse_position = event.position
 
 
 
