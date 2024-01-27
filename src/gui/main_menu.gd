480e46e6b0095b91e7b6368de982e8d5c7da76fd
Theo <theonixie@gmail.com>
2024-01-27 12:08:01 -0800

    More polish to title screen

--- src/gui/main_menu.gd
+++ src/gui/main_menu.gd
@@ -5,6 +5,11 @@ signal new_drawing()
 
 func _ready():
 	pass # Replace with function body.
+	
+func _process(delta):
+	$BackgroundSpikes.region_rect.position.y -= delta * 8
+	$BackgroundConfetti1.region_rect.position.y -= delta * 6
+	$BackgroundConfetti2.region_rect.position.y -= delta * 3
 
 func _on_new_drawing_button_pressed():
 	new_drawing.emit()
