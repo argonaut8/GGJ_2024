4c79e0621e8b5c0854d0e71c551d2a312d9f2664
Theo <theonixie@gmail.com>
2024-01-28 12:06:17 -0800

    Add save functionality

--- src/gui/final_screen.gd
+++ src/gui/final_screen.gd
@@ -22,9 +22,18 @@ func set_gradient(canvas_gradient: Image) -> void:
 func set_prompt(prompt: String) -> void:
 	prompt_label.text = prompt
 
+func _on_save_button_pressed():
+	# For some reason we need to wait two frames for the viewport.
+	await get_tree().idle_frame
+	await get_tree().idle_frame
+	var rectangle = Rect2(0, 0, 250, 300)
+	var img = %SubViewport.get_texture().get_data() as Image
+	img.flip_y()
+	var buffer = img.save_png_to_buffer
+	JavaScriptBridge.download_buffer(buffer, "render.png")
+
 func _on_restart_button_pressed():
 	restart_draw.emit()
-
 
 func _on_main_menu_button_pressed():
 	main_menu_requested.emit()
